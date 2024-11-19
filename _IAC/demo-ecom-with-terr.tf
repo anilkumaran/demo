6c3c504b8e1ac8233c3d828eeff9024f4ffb269c
Anil Koppula <72085415+anilkumaran@users.noreply.github.com>
2024-11-19 10:16:16 +0530

    Update demo-ecom-with-terr.tf

--- _IAC/demo-ecom-with-terr.tf
+++ _IAC/demo-ecom-with-terr.tf
@@ -6,3 +6,12 @@ resource "aws_s3_bucket" "demo-bucket" {
     app         = "demo-ecom-app"
   }
 }
+
+locals {
+  users = ["Todd", "James", "Alice", "Dottie"]
+}
+
+resource "aws_iam_user" "the-accounts" {
+  for_each = toset(local.users)
+  name     = each.key
+}
